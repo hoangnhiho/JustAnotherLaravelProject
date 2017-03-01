1e3e26915fc0ba15b4a584b64af93e354fa6bc5c
Hoang Ho <hoang.ho@uqconnect.edu.au>
2017-03-01 22:51:57 +1000

    img delay

--- resources/views/frontend/frontPage.blade.php
+++ resources/views/frontend/frontPage.blade.php
@@ -39,7 +39,7 @@
 
     <br>
 
-    <section id="">
+    <section>
         <div class="container wow fadeInDown">
             <div class="row">
                 <div class="col-sm-offset-1 col-sm-6 col-xs-12 listing-preview listing-preview-top" id="listing-preview-top-left" style="">
@@ -72,19 +72,30 @@
 
     <br>
 
-    <section id="">
-        <div class="row wow fadeInDown" style="margin: 0px 0px; background: url({{ url('images/frontpage/world_map.png') }}) no-repeat center center; background-size: cover;">
-            <div class="col-sm-offset-1 col-sm-5 col-xs-12" style="padding: 0px 0px; margin-top: 20px; margin-bottom: 20px;">
-                <!-- <div style="height:300px;"></div> -->
-                <div class="house-content">
-                    <div class="house-text" style="padding: 10px 10px; color:purple">
-                        <h2 style="color:purple">LOCALLY ESTABLISHED GLOBALLY CONNECTED</h2>
-                        <p>Our objective as a client-focused consultancy Group is to link the right investors to the right prperty. We enable businesses, individuals and investors to reach their financial goals, and are committed to acquire and develop quality properties based on realistic values for our clients.</p>
+        <div class="container wow fadeInDown" style="height: 400px;">
+            <div class="row wow fadeInDown overlap-img animated" style="height: 400px; margin: 0px 0px; background: url({{ url('images/frontpage/worldmap/worldmap.png') }}) no-repeat center center; background-size: cover;">
+                <div class="col-sm-offset-1 col-sm-5 col-xs-12" style="padding: 0px 0px; margin-top: 20px; margin-bottom: 20px;">
+                    <!-- <div style="height:300px;"></div> -->
+                    <div class="house-content">
+                        <div class="house-text" style="padding: 10px 10px; color:purple">
+                            <h2 style="color:purple">LOCALLY ESTABLISHED GLOBALLY CONNECTED</h2>
+                            <p>Our objective as a client-focused consultancy Group is to link the right investors to the right prperty. We enable businesses, individuals and investors to reach their financial goals, and are committed to acquire and develop quality properties based on realistic values for our clients.</p>
+                        </div>
                     </div>
                 </div>
+
             </div>
+            <div class="row wow fadeInDown overlap-img animated" data-wow-delay="400ms" style="height: 400px; margin: 0px 0px; background: url({{ url('images/frontpage/worldmap/Japan.png') }}) no-repeat center center; background-size: cover;"></div>
+            <div class="row wow fadeInDown overlap-img animated" data-wow-delay="600ms" style="height: 400px; margin: 0px 0px; background: url({{ url('images/frontpage/worldmap/S-Korea.png') }}) no-repeat center center; background-size: cover;"></div>
+            <div class="row wow fadeInDown overlap-img animated" data-wow-delay="800ms" style="height: 400px; margin: 0px 0px; background: url({{ url('images/frontpage/worldmap/China.png') }}) no-repeat center center; background-size: cover;"></div>
+            <div class="row wow fadeInDown overlap-img animated" data-wow-delay="1000ms" style="height: 400px; margin: 0px 0px; background: url({{ url('images/frontpage/worldmap/Taiwan.png') }}) no-repeat center center; background-size: cover;"></div>
+            <div class="row wow fadeInDown overlap-img animated" data-wow-delay="1200ms" style="height: 400px; margin: 0px 0px; background: url({{ url('images/frontpage/worldmap/Hong-Kong.png') }}) no-repeat center center; background-size: cover;"></div>
+            <div class="row wow fadeInDown overlap-img animated" data-wow-delay="1400ms" style="height: 400px; margin: 0px 0px; background: url({{ url('images/frontpage/worldmap/Thailand.png') }}) no-repeat center center; background-size: cover;"></div>
+            <div class="row wow fadeInDown overlap-img animated" data-wow-delay="16000ms" style="height: 400px; margin: 0px 0px; background: url({{ url('images/frontpage/worldmap/Vietnam.png') }}) no-repeat center center; background-size: cover;"></div>
+            <div class="row wow fadeInDown overlap-img animated" data-wow-delay="1800ms" style="height: 400px; margin: 0px 0px; background: url({{ url('images/frontpage/worldmap/Malaysia.png') }}) no-repeat center center; background-size: cover;"></div>
+            <div class="row wow fadeInDown overlap-img animated" data-wow-delay="2000ms" style="height: 400px; margin: 0px 0px; background: url({{ url('images/frontpage/worldmap/Singapore.png') }}) no-repeat center center; background-size: cover;"></div>
+            <div class="row wow fadeInDown overlap-img animated" data-wow-delay="2200ms" style="height: 400px; margin: 0px 0px; background: url({{ url('images/frontpage/worldmap/Indonesia.png') }}) no-repeat center center; background-size: cover;"></div>
         </div>
-    </section>
 
     <br>
     
